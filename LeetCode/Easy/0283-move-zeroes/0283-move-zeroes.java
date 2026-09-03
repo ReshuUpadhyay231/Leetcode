53beee2e33a0763236f1fd8d1f325f35c3f70b5d
Reshu Upadhyay <upadhyayreshu726@gmail.com>
2026-09-03 19:47:51 +0530

    Time: 2 ms (91.84%), Space: 47.8 MB (38.33%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
@@ -3,9 +3,9 @@ class Solution {
         int st=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[st];
-                nums[st]=temp;
+                int temp=nums[st];
+                nums[st]=nums[i];
+                nums[i]=temp;
                 st++;
             }
         }
